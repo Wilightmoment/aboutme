a95bcdba5e4188ec065890235959e133abdb322d
Wilightmoment <a777441a@gmail.com>
2018-09-09 17:54:07 +0800

    ver.0.8
    1. 大幅更新畫面
    2. 暫時取消sidebar
    3. 作品尚未更新

--- js/main.js
+++ js/main.js
@@ -1,41 +1,74 @@
 $(document).ready(function(){
+	// tooltip
+	$(function () {
+		$('[data-toggle="tooltip"]').tooltip()
+	});
+
+	$('.parallax-window').parallax({imageSrc: '/image/window01.jpg'});
 	console.log('ready');
 	$(document).on("scroll", onScroll);
+	
 
 	$('a[href^="#"]').on('click', function(e) {
-		e.preventDefault();
+		e.preventDefault();		
 		$(document).off("scroll");
 
 		$('a').each(function() {
-			$(this).removeClass('active');
+			// $(this).removeClass('active');
+			if($(this).parent().hasClass('nav-item')){
+				$(this).parent().removeClass('active')
+			}
 		})
-		$(this).addClass('active')
+		// $(this).addClass('active')
 
-		
-        let target = this.hash;
+		$(this).each(function(){
+			if($(this).parent().hasClass('nav-item')){
+				$(this).parent().addClass('active')
+			}
+		})
+
+		let target = this.hash;
+		$target = $(target);
         $('html, body').stop().animate({
-            'scrollTop': $(target).offset().top+2
-        }, 500, 'swing', function () {
-            window.location.hash = target;
+            'scrollTop': $target.offset().top-100
+        }, 550, 'swing', function () {
+			window.location.hash = target+100;
+			console.log(target)
             $(document).on("scroll", onScroll);
         });
-	});
+	});	
+	
 });
 
 // 針對目前位置對sidebar做出變化
 function onScroll(event){
 	let scrollPos = $(document).scrollTop();
-	console.log('scrollPos', scrollPos)
-	$(".list-group>li>a").each(function() {
+	// console.log('scrollPos', scrollPos)
+	//側選單
+	// $(".list-group>li>a").each(function() {
+	// 	let currentLink = $(this);
+	// 	let refElement = $(currentLink.attr('href'));
+	// 	console.log('refElement.postion', refElement.position().top)
+	// 	console.log('refElement.height', refElement.height())
+	// 	if(refElement.position().top <= scrollPos-600 && refElement.position().top + refElement.height() > scrollPos-600){
+	// 		$('.list-group>li>a').removeClass('active');
+	// 		currentLink.addClass('active')
+	// 	}else{
+	// 		currentLink.removeClass('active')
+	// 	}
+	// });
+
+	//上方導覽列
+	$(".navbar-nav>li>a").each(function() {
 		let currentLink = $(this);
 		let refElement = $(currentLink.attr('href'));
 		console.log('refElement.postion', refElement.position().top)
 		console.log('refElement.height', refElement.height())
-		if(refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos){
-			$('.list-group>li>a').removeClass('active');
-			currentLink.addClass('active')
+		if(refElement.position().top <= scrollPos-600 && refElement.position().top + refElement.height() > scrollPos-600){
+			$('.navbar-nav>li').removeClass('active');
+			currentLink.parent().addClass('active')
 		}else{
-			currentLink.removeClass('active')
+			currentLink.parent().removeClass('active')
 		}
 	});
 }
